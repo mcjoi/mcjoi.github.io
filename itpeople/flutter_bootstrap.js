8d1b30e384aba39d2c98d66ba8057081fc90cde7
Joi <31478078+mcjoi@users.noreply.github.com>
2024-10-16 18:46:06 +0900

    Add files via upload

--- itpeople/flutter_bootstrap.js
+++ itpeople/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "825527495"
+    serviceWorkerVersion: "1146223483"
   }
 });
