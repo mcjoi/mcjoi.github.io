88cb2058b011596e0c6b069fd02a023204edaec5
Joi <31478078+mcjoi@users.noreply.github.com>
2025-12-25 10:30:03 +0900

    2025-12-25 10:30:03

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2782921507"
+    serviceWorkerVersion: "2209320306"
   }
 });
